c9fc396f9c76a584481b6e3a223c529b07902700
David Khristepher Santos <rupert.avery@gmail.com>
2020-12-15 00:23:15 +0800

    Proper fix for AppPath

--- PSXPackager/ApplicationInfo.cs
+++ PSXPackager/ApplicationInfo.cs
@@ -1,15 +1,15 @@
-﻿namespace PSXPackager
+﻿using System;
+using System.IO;
+
+namespace PSXPackager
 {
     public static class ApplicationInfo
     {
         public static string AppPath { get; private set; }
+
         static ApplicationInfo()
         {
-            // Not working in WSL/Mono
-            //string path = System.Reflection.Assembly.GetExecutingAssembly().CodeBase.Replace("file:\\\\\\", "").Replace("file:///", "");
-            //AppPath = System.IO.Path.GetDirectoryName(path);
-            AppPath = ".";
+            AppPath = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
         }
-
     }
 }
